136a3b881bd923f3d27a6fce5a801c19b301fa8a
Georgiy Rusakov <gosha1510@yandex.ru>
2023-10-23 21:51:29 +0300

    Add homework 3

---
+++ home-work-3/src/main/java/ru/otus/spring/homework3/HomeWork3Application.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.homework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork3Application.class, args);
+	}
+
+}
---
+++ home-work-3/src/test/java/ru/otus/spring/homework3/HomeWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.homework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
